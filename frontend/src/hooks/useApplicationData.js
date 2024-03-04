325b56f6df9b94b2557133ce099be0a7b181f04a
hashbrwn <abdihashi2001@hotmail.com>
2024-03-04 09:24:23 -0500

    Added custom hook useApplicationData for managing state and actions related to photo data and topics

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,52 @@
+import { useReducer } from "react";
+
+import reducer, {
+  PHOTO_LIKE_BTN_CLICK,
+  SHOW_MODAL,
+  CLOSE_MODAL,
+  SET_APPLICATION_DATA,
+  GET_TOPIC_PHOTOS
+} from "../reducers/photoReducers";
+
+export default function useApplicationData() {
+  const defaultState = {
+    isModalOpen: false,
+    modalPhotoDetails: null,
+    topics: [],
+    photos: [],
+    favPhotoList: [],
+  };
+
+
+
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const photoFavBtnClicked = (id) => {
+    dispatch({ type: PHOTO_LIKE_BTN_CLICK, payload: id });
+  };
+
+  const showModal = (id) => {
+    dispatch({ type: SHOW_MODAL, payload: id });
+  };
+
+  const closeModal = () => {
+    dispatch({ type: CLOSE_MODAL });
+  };
+
+  const setAppData = (topics, photos) => {
+    dispatch({ type: SET_APPLICATION_DATA, payload: { topics, photos } });
+  }
+
+  const getTopicPhotos = (id) => {
+    dispatch({ type: GET_TOPIC_PHOTOS, payload: id })
+  }
+
+  return {
+    state,
+    photoFavBtnClicked,
+    showModal,
+    closeModal,
+    setAppData,
+    getTopicPhotos
+  };
+}
