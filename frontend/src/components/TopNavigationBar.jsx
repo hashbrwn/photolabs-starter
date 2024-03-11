c6feb4f8e015866857b5bc5eb60816354a554a6f
hashbrwn <abdihashi2001@hotmail.com>
2024-03-11 16:01:16 -0400

    heart icon that changes state whether there is at least 1 favourited photo

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-
 import "styles/TopNavigationBar.scss";
 import TopicList from "components/TopicList";
 import FavBadge from "./FavBadge";
 
 const TopNavigation = (props) => {
   const { favPhotoList, topics, getTopicPhotos } = props;
-  console.log('favPhotoList', favPhotoList)
   return (
-    <>
-      <div className="top-nav-bar">
-        <span className="top-nav-bar--logo">Photo-Labs</span>
-        <TopicList
-          favPhotoList={favPhotoList}
-          topics={topics}
-          getTopicPhotos={getTopicPhotos}
-        />
-        <FavBadge isFavPhotoExist ={favPhotoList.length < 0}/> 
-      </div>
-    </>
+    <div className="top-nav-bar">
+      <span className="top-nav-bar__logo">Photo-Labs</span>
+      <TopicList
+        favPhotoList={favPhotoList}
+        topics={topics}
+        getTopicPhotos={getTopicPhotos}
+      />
+      <FavBadge isFavPhotoExist={favPhotoList.length > 0}/> 
+    </div>
   );
 };
 
